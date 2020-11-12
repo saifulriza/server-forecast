30fe05c8658959b7601672a5b421a8fae2b39d59
saifulriza123 <mail.saifulriza@gmail.com>
2020-11-13 02:08:19 +0700

    setup port

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
     module.hot.accept();
